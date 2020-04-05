e37cda7d0c0eb354302e5db7dc4121617e0e74f6
Mui <public.mui@gmail.com>
2020-04-05 14:24:13 +0900

    ABC153 problem  comment added

--- atcoder/abc153/d.c
+++ atcoder/abc153/d.c
@@ -1,13 +1,18 @@
+/*
+ * D - Caracal vs Monster
+ * @author: Mui
+ * @date: April 5th, 2020
+ * @ref: https://atcoder.jp/contests/abc153/tasks/abc153_d
+ */
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
 int main(void)
 {
-    long h;
-    scanf("%ld", &h);
-    int a = log(h) / log(2) + 1;
-    long b = pow(2, a) - 1;
-    printf("%ld\n", b);
-    return 0;
+	long h;
+	scanf("%ld", &h);
+    h = log(h) / log(2) + 1;
+	printf("%ld\n", ((long)1 << h)-1);
+	return 0;
 }
