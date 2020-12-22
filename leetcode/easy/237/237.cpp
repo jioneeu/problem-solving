e09fe8defafeccc66868f7b983e72caf17c34038
j2ieu <j2ieu@pm.me>
2020-12-22 17:40:13 +0900

    Another solution added

--- leetcode/easy/237/237.cpp
+++ leetcode/easy/237/237.cpp
@@ -20,3 +20,18 @@ class Solution {
       delete temp;
     }
 };
+
+/**
+ * Runtime: 20 ms, faster than 53.79%
+ * Memory Usage: 8 MB, less than 52.59%
+ */
+
+class Solution {
+  public:
+    void deleteNode(ListNode* node) {
+      node->val = node->next->val;
+      ListNode *temp = node->next;
+      node->next = node->next->next;
+      delete temp;
+    }
+};
